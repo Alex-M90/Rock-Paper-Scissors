713af9c5a0a4d98617562faf58bff1d94eac35e7
Alex Martinig <alex.martinig@calibre.org.uk>
2022-07-14 16:09:08 +0100

    added more functions and variables to complete the assignment

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 I am creating a Rock Paper Scissors game as part of the Foundations course on [The Odin Project](https://www.theodinproject.com/).
 
 - Added a function (computerPlay) to randomly generate string from an array (choice).
-- Added another function with an if else statement to check between player and computer choice, determining which is the winner of each game.
+- Added another function (playRound) with an if else statement to check between player and computer choice, determining which is the winner of each game.
+- Created variables to hold the score from the user, computer and draws.
+- Created variables to display the messages for the various results.
+- Created a function (game) that had a for loop in it, which prompted the player for their selection, called in the (computerPlay), then compared the results from each round (playRound) and added it to the players/computers score, which was logged. Then added an if/else statement to compare the scores and log the winner.
--- script.js
+++ script.js
@@ -1,25 +1,62 @@
-let choice = ['rock', 'paper', 'scissors'];
+//to store the scores
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+
+//Computer choice section
+let choice = ["rock", "paper", "scissors"];
 
 let computerPlay = (computerChoice) => {
-    computerChoice = choice[Math.floor(Math.random() * choice.length)];
-    return computerChoice;
+  computerChoice = choice[Math.floor(Math.random() * choice.length)];
+  return computerChoice;
 };
 
+//messages for the results
+let playerWinRound = "You win this round!";
+let computerWinRound = "Your opponent wins this round!";
+let draw = "It's a draw!";
+let playerWin = "Congratulations, you've won!";
+let computerWin = "Bad luck, your opponent won!";
+let totalDraw = "You're evenly matched, it's a draw!";
+
+//Function to play through a round and return the winner of each
 let playRound = (playerSelection, computerSelection) => {
-
-    if (playerSelection === computerSelection) {
-        return "It's a draw!"
-    } else if ((playerSelection === "paper") && (computerSelection === "rock")){
-        return "You win! Paper beats rock."
-    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
-        return "You win! Rock beats scissors."
-    } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        return "You win! Scissors beats paper."
-    } else {
-        return "You lose! Try again."
-    }
+  if (playerSelection === computerSelection) {
+    return draw;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return playerWinRound; 
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return playerWinRound;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return playerWinRound;
+  } else {
+    return computerWinRound;
+  }
 };
 
-const playerSelection = "paper";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+//Function for the game to loop through 5 rounds, then adding results to the totals before logging the scores and final results.
+let game = () => {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Choose your weapon. Rock, Paper or Scissors?").toLowerCase();
+    const computerSelection = computerPlay();
+    let roundResult = playRound(playerSelection, computerSelection);
+     if (roundResult === playerWinRound) {
+       playerScore++;
+     } else if (roundResult === draw) {
+       draws++;
+     } else {
+       computerScore++;
+     }
+    console.log(roundResult);
+    console.log(`Your score is ${playerScore}`);
+    console.log(`Your opponents score is ${computerScore}`);
+  };
+
+  if (playerScore > computerScore) {
+    return playerWin;
+  } else if (playerScore === computerScore) {
+    return totalDraw;
+  } else {
+    return computerWin;
+  }
+};
